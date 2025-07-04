747a9ba5ad8d113cbcbd16228a63eede8a4801b5
SohamJoshi25 <sohamjoshichinchwad@gmail.com>
2025-07-04 10:56:54 +0530

    github link change

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,5 +1,5 @@
 export const BACKEND_DOMAIN = process.env.NODE_ENV === "production" ? "https://driveconnect-backend.onrender.com" : "http://localhost:8080";
-export const GITHUB_LINK = "https://github.com/SohamJoshi25/driveconnect-frontend";
+export const GITHUB_LINK = "https://github.com/SohamJoshi25/driveconnect";
 export const LINKEDIN_LINK = "https://www.linkedin.com/in/sohamjoshi25/";
 export const CONTACT_MAIL = "contact@sohamjoshi.in";
 export const WEBSITE_LINK = "https://www.sohamjoshi.in";;
